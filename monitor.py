4316c81fb100209e632a45f443a6b2a91aa3c55c
venkamita <doddihalsharat@gmail.com>
2026-04-12 13:28:04 -0400

    Added monitor controller

---
+++ monitor.py
@@ -0,0 +1,40 @@
+from ryu.base import app_manager
+from ryu.controller import ofp_event
+from ryu.controller.handler import MAIN_DISPATCHER, DEAD_DISPATCHER, CONFIG_DISPATCHER
+from ryu.controller.handler import set_ev_cls
+from ryu.ofproto import ofproto_v1_3
+from ryu.lib import hub
+
+class Monitor(app_manager.RyuApp):
+    OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
+
+    def __init__(self, *args, **kwargs):
+        super(Monitor, self).__init__(*args, **kwargs)
+        self.datapaths = {}
+        self.monitor_thread = hub.spawn(self._monitor)
+
+    @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
+    def switch_features_handler(self, ev):
+        datapath = ev.msg.datapath
+        self.datapaths[datapath.id] = datapath
+
+    def _monitor(self):
+        while True:
+            for dp in self.datapaths.values():
+                self.request_stats(dp)
+            hub.sleep(2)
+
+    def request_stats(self, datapath):
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+
+        req = parser.OFPPortStatsRequest(datapath, 0, ofproto.OFPP_ANY)
+        datapath.send_msg(req)
+
+    @set_ev_cls(ofp_event.EventOFPPortStatsReply, MAIN_DISPATCHER)
+    def port_stats_reply_handler(self, ev):
+        body = ev.msg.body
+
+        print("\n--- Port Stats ---")
+        for stat in body:
+            print(f"Port {stat.port_no} RX_bytes={stat.rx_bytes} TX_bytes={stat.tx_bytes}")
