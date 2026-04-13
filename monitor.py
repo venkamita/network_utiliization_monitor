3c0fe6f2a637535886b8aba98d4e195849ac8478
venkamita <doddihalsharat@gmail.com>
2026-04-13 03:13:11 -0400

    Updated project files

--- monitor.py
+++ monitor.py
@@ -1,15 +1,17 @@
 from ryu.base import app_manager
 from ryu.controller import ofp_event
-from ryu.controller.handler import MAIN_DISPATCHER, DEAD_DISPATCHER, CONFIG_DISPATCHER
+from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER
 from ryu.controller.handler import set_ev_cls
 from ryu.ofproto import ofproto_v1_3
+from ryu.lib.packet import packet, ethernet
 from ryu.lib import hub
 
-class Monitor(app_manager.RyuApp):
+class MonitorSwitch(app_manager.RyuApp):
     OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
 
     def __init__(self, *args, **kwargs):
-        super(Monitor, self).__init__(*args, **kwargs)
+        super(MonitorSwitch, self).__init__(*args, **kwargs)
+        self.mac_to_port = {}
         self.datapaths = {}
         self.monitor_thread = hub.spawn(self._monitor)
 
@@ -18,6 +20,68 @@ class Monitor(app_manager.RyuApp):
         datapath = ev.msg.datapath
         self.datapaths[datapath.id] = datapath
 
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+
+        # Table-miss rule (send unknown packets to controller)
+        match = parser.OFPMatch()
+        actions = [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER)]
+        self.add_flow(datapath, 0, match, actions)
+
+    def add_flow(self, datapath, priority, match, actions):
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+
+        inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS, actions)]
+        mod = parser.OFPFlowMod(datapath=datapath, priority=priority,
+                               match=match, instructions=inst)
+        datapath.send_msg(mod)
+
+    @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
+    def packet_in_handler(self, ev):
+        msg = ev.msg
+        datapath = msg.datapath
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+
+        dpid = datapath.id
+        self.mac_to_port.setdefault(dpid, {})
+
+        pkt = packet.Packet(msg.data)
+        eth = pkt.get_protocol(ethernet.ethernet)
+
+        dst = eth.dst
+        src = eth.src
+        in_port = msg.match['in_port']
+
+        # Learn MAC address
+        self.mac_to_port[dpid][src] = in_port
+
+        # Decide output port
+        if dst in self.mac_to_port[dpid]:
+            out_port = self.mac_to_port[dpid][dst]
+        else:
+            out_port = ofproto.OFPP_FLOOD
+
+        actions = [parser.OFPActionOutput(out_port)]
+
+        # Install flow rule
+        if out_port != ofproto.OFPP_FLOOD:
+            match = parser.OFPMatch(in_port=in_port, eth_dst=dst)
+            self.add_flow(datapath, 1, match, actions)
+
+        # Send packet (IMPORTANT FIX INCLUDED)
+        data = None
+        if msg.buffer_id == ofproto.OFP_NO_BUFFER:
+            data = msg.data
+
+        out = parser.OFPPacketOut(datapath=datapath,
+                                 buffer_id=msg.buffer_id,
+                                 in_port=in_port,
+                                 actions=actions,
+                                 data=data)
+        datapath.send_msg(out)
+
     def _monitor(self):
         while True:
             for dp in self.datapaths.values():
@@ -25,16 +89,14 @@ class Monitor(app_manager.RyuApp):
             hub.sleep(2)
 
     def request_stats(self, datapath):
-        ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
+        ofproto = datapath.ofproto
 
         req = parser.OFPPortStatsRequest(datapath, 0, ofproto.OFPP_ANY)
         datapath.send_msg(req)
 
     @set_ev_cls(ofp_event.EventOFPPortStatsReply, MAIN_DISPATCHER)
     def port_stats_reply_handler(self, ev):
-        body = ev.msg.body
-
         print("\n--- Port Stats ---")
-        for stat in body:
-            print(f"Port {stat.port_no} RX_bytes={stat.rx_bytes} TX_bytes={stat.tx_bytes}")
+        for stat in ev.msg.body:
+            print(f"Port {stat.port_no} RX={stat.rx_bytes} TX={stat.tx_bytes}")
